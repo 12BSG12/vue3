678fa35c5b3186683966e7473c8f0709fd0ec26f
12BSG12 <carlj8335@gmail.com>
2023-02-28 18:29:51 +0500

    fix YmapPlugin

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,15 @@ import './main.scss';
 import vuetify from './plugins/vuetify';
 
 const app = createApp(App);
+const yandexApiKey = import.meta.env.VITE_YANDEX_API_KEY;
 
+const settings = {
+  apiKey: yandexApiKey, // Индивидуальный ключ API
+  lang: 'ru_RU', // Используемый язык
+  coordorder: 'latlong', // Порядок задания географических координат
+  debug: false, // Режим отладки
+  version: '2.1', // Версия Я.Карт
+};
 
 directives.forEach((directive) => {
   app.directive(directive.name, directive);
@@ -21,6 +29,6 @@ app.use(createPinia());
 app.use(router);
 app.use(vuetify);
 app.use(VueQueryPlugin);
-app.use(YmapPlugin);
+app.use(YmapPlugin, settings);
 
 app.mount('#app');
--- src/views/map/MapView.vue
+++ src/views/map/MapView.vue
@@ -5,15 +5,7 @@ const coordinates = [56.76446938396302, 54.159877347983766];
 const zoom = 17;
 const controls = ['zoomControl'];
 const detailedControls = { zoomControl: { position: { right: 10, top: 100 } } };
-const yandexApiKey = import.meta.env.VITE_YANDEX_API_KEY;
 
-const settings = {
-  apiKey: yandexApiKey, // Индивидуальный ключ API
-  lang: 'ru_RU', // Используемый язык
-  coordorder: 'latlong', // Порядок задания географических координат
-  debug: false, // Режим отладки
-  version: '2.1', // Версия Я.Карт
-};
 </script>
 
 <template>
@@ -24,7 +16,6 @@ const settings = {
         <v-divider></v-divider>
         <YandexMap
           class="mt-5"
-          :settings="settings"
           :coordinates="coordinates"
           :zoom="zoom"
           :controls="controls"
